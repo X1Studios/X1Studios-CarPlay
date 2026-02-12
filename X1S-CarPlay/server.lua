aa6fece5dbcb90ea73bf1b62550320488b770ec5
Havoc Sheriff <sheriffx1studios@gmail.com>
2026-02-11 17:36:59 -0700

    Update server.lua

--- X1S-CarPlay/server.lua
+++ X1S-CarPlay/server.lua
@@ -18,7 +18,6 @@ RegisterNetEvent('x1s:playSong', function(data)
     local net = data.net
     local link = data.link
 
-    -- 🔥 REPLACE current song instead of queueing
     queues[net] = { link }
 
     TriggerClientEvent('x1s:syncSong', -1, {
@@ -51,4 +50,36 @@ CreateThread(function()
             end
         end
     end
+end)
+
+local resourceName = GetCurrentResourceName()
+local currentVersion = GetResourceMetadata(resourceName, 'version', 0)
+
+local versionUrl = "https://github.com/X1Studios/X1Studios-CarPlay/blob/main/version.txt"
+
+local function CheckForUpdates()
+    PerformHttpRequest(versionUrl, function(statusCode, responseText, headers)
+        if statusCode ~= 200 then
+            print("^1[Update Checker] Unable to check for updates (HTTP Error " .. statusCode .. ")^0")
+            return
+        end
+
+        local latestVersion = responseText:gsub("%s+", "")
+
+        if latestVersion == currentVersion then
+            print("^2[Update Checker] " .. resourceName .. " is up to date! (v" .. currentVersion .. ")^0")
+        else
+            print("^3-------------------------------------------------------^0")
+            print("^1[Update Checker] Update Available for " .. resourceName .. "!^0")
+            print("^3Current Version:^0 " .. currentVersion)
+            print("^2Latest Version:^0 " .. latestVersion)
+            print("^5Download:^0 https://github.com/YourName/YourRepo")
+            print("^3-------------------------------------------------------^0")
+        end
+    end, "GET")
+end
+
+CreateThread(function()
+    Wait(3000)
+    CheckForUpdates()
 end)
