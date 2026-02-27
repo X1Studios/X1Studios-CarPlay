8ab399494327205e4ebd29a7c96797a7110fb53b
Havoc Sheriff <sheriffx1studios@gmail.com>
2026-02-26 17:45:24 -0700

    Bump version to 1.1.0 and add image assets
    Updated version to 1.1.0 and added new image files.

--- X1S-CarPlay/fxmanifest.lua
+++ X1S-CarPlay/fxmanifest.lua
@@ -3,14 +3,16 @@ game 'gta5'
 
 author 'X1Studios'
 description 'CarPlay'
-version '1.0.1'
+version '1.1.0'
 
 ui_page 'html/index.html'
 
 files {
     'html/index.html',
     'html/style.css',
-    'html/app.js'
+    'html/app.js',
+    'html/img/skip-red.png',
+    'html/img/skip-black.png'
 }
 
 client_scripts {
@@ -19,4 +21,3 @@ client_scripts {
 }
 
 server_script 'server.lua'
-
